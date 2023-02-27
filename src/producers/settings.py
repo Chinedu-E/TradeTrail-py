d4d9e88b62c367c19a03e1dffb2a2bd8efd9b314
chinedu <crankyekeruche@gmail.com>
2023-02-27 16:22:14 -0600

    added type annotation for spawn_channel

--- src/producers/settings.py
+++ src/producers/settings.py
@@ -1,8 +1,9 @@
 import pika
+from pika.adapters.blocking_connection import BlockingChannel
 
 RABBIT_CONNECTION = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
-def spawn_channel(queue_name: str):
+def spawn_channel(queue_name: str) -> BlockingChannel:
     channel = RABBIT_CONNECTION.channel()
     channel.queue_declare(queue=queue_name)
     return channel
