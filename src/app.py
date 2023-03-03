466c9f3a79ff29edbf29a42d2523740aad302229
chinedu <crankyekeruche@gmail.com>
2023-03-02 22:07:53 -0600

    added sentiment endpoint

--- src/app.py
+++ src/app.py
@@ -8,16 +8,26 @@ from fastapi.websockets import WebSocket
 from helpers import Participant, ParticipantSession, SessionsManager, Session
 from producers import spawn_channel
 from trading import spawn_bots
+from sentiment import NewsPipeline, TwitterPipeline
 
 CHANNEL_NAME = "session"
 app = FastAPI()
 session_manager = SessionsManager()
+twitter_pipeline = TwitterPipeline()
+news_pipeline = NewsPipeline()
+
 
 
 @app.get("/")
 async def health_check():
     return "Server running"
 
+@app.get("/sentiment")
+def get_sentiment(ticker: str):
+    response = {}
+    response["twitter"] = twitter_pipeline(ticker=ticker)
+    response["news"] = news_pipeline(ticker=ticker)
+    return response
 
 @app.websocket("/ws/create")
 async def create_session(websocket: WebSocket, host_id: int, symbol:str, id: int,
@@ -29,7 +39,7 @@ async def create_session(websocket: WebSocket, host_id: int, symbol:str, id: int
     await session_manager.add_session(session)
     await session_manager.add_client((websocket, host_id))
     
-    session_participant = ParticipantSession(starting_balance=starting_balance)
+    session_participant = ParticipantSession(starting_balance=starting_balance, is_agent=False)
     
     if against_server:
         num_bots = max_clients - 1
@@ -54,13 +64,13 @@ async def create_session(websocket: WebSocket, host_id: int, symbol:str, id: int
         
     
 @app.websocket("/ws/join")
-async def join_session(websocket: WebSocket, session_id: int, client_id: int):
+async def join_session(websocket: WebSocket, session_id: int, client_id: int, is_agent: bool):
     await websocket.accept()
     
     await session_manager.add_client(websocket, session_id, client_id)
     
     session: Session = session_manager[session_id]
-    session_participant = ParticipantSession(starting_balance=session.starting_balance)
+    session_participant = ParticipantSession(starting_balance=session.starting_balance, is_agent=is_agent)
     
     while not session_manager.is_done(session_id):
         text = await websocket.receive()
