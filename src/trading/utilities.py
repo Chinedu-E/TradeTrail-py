fff84928b6898e74f3db8f0c788e3d8199a6dbee
chinedu <crankyekeruche@gmail.com>
2023-03-05 23:44:07 -0600

    removed get_data func, added form_labels

--- src/trading/utilities.py
+++ src/trading/utilities.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+from typing import List
 
 import numpy as np
 import pandas as pd
@@ -8,8 +9,9 @@ from finta import TA
 
 PREDICTORS = ["Open", "High", "Low", "Close", "Volume", "SMA", "RSI", "OBV", "KAMA", "ROC"]
 
-def form_features(ticker: str, df: pd.DataFrame = None):
+def form_features(ticker: str = None, df: pd.DataFrame = None):
     if df is None:
+        assert ticker is not None
         df = yf.download(ticker, interval="1m", period="5d")
     df['SMA'] = TA.SMA(df, 30)
     df['RSI'] = TA.RSI(df)
@@ -20,16 +22,8 @@ def form_features(ticker: str, df: pd.DataFrame = None):
     df= df[PREDICTORS]
     return df
 
-def get_data(symbol: str):
-    main_df = pd.DataFrame()
-    for i in reversed(range(1, 3)):
-        CSV_URL = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol={symbol}&interval=1min&slice=year2month{i}&apikey='
-        with requests.Session() as s:
-            download = s.get(CSV_URL)
-            decoded_content = download.content.decode('utf-8')
-            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-            my_list = list(cr)
-            df = pd.DataFrame(my_list[1:], columns=my_list[0])
-        main_df = pd.concat([df, main_df])
-    return main_df
 
+def form_labels(df: pd.DataFrame) -> List[str]:
+    df["Tomorrow"] = df["Close"].shift(-1)
+    df["Target"] = (df["Tomorrow"] > df["Close"]).astype(int)
+    return list(df["Target"].values)
